0435db06e8a361a00101c490f54139982f323644
andywildridge <andy.wildridge@gmail.com>
2021-04-07 17:22:20 +0100

    added base classes and mobile demo html

--- shapes/src/components/shape.js
+++ shapes/src/components/shape.js
@@ -1,3 +1,42 @@
-export const shape = () => {
-    return 'shape';
+import shapeDefinitions from './shapeDefinitions.js';
+import getShapeImage from './getShapeImage.js';
+
+export class Shape {
+    constructor(type, name) {
+        this._defn = shapeDefinitions[type];
+        this._img = getShapeImage(this._defn);
+        this._position = undefined;
+        this._name = name;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    set position({
+        x = this.position?.x,
+        y = this.position?.y,
+        rotate = this.position?.rotate,
+        flip = this.position?.flip,
+    }) {
+        this._position = {
+            x,
+            y,
+            rotate,
+            flip,
+        };
+    }
+
+    get img() {
+        return this._img;
+    }
+
+    get name() {
+        return this._name;;
+    }
+
+    setEl(el) {
+        this._el = el;
+        console.log(el);
+    }
 }
